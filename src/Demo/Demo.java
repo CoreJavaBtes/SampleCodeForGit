febebfb371b0ecaa6335d780f692c1627a9ba737
Sipika <CoreJavaBtes@gmail.com>
2018-01-15 12:34:51 +0530

    commit

--- src/Demo/Demo.java
+++ src/Demo/Demo.java
@@ -1,7 +1,23 @@
 package Demo;
 
+import java.util.Scanner;
+
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("hello");
+	System.out.println("Please enter any Number ");
+	Scanner sn = new Scanner(System.in);
+	int var = sn.nextInt();
+	int i = 0;
+	do{
+		System.out.println(i);
+		i++;
+	}while(i<var);
+	System.out.println("---------------------");
+	for(int j = 0; j<var; j++)
+	{
+		System.out.println(j);
+	}
+	
+	
 }
 }
